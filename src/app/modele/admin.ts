46d2734e5701b26e2c3d42ab0330f369dd6106cd
rahma-romdhani2022 <rahma.github2022@gmail.com>
2022-05-01 11:13:10 +0100

    first commit

---
+++ src/app/modele/admin.ts
@@ -0,0 +1,10 @@
+export interface Admin {
+    id : number ;
+    username :string ; 
+    email : string ; 
+    image : string ;
+    password : string ; 
+    diplome : string ; 
+    role :string ; 
+    accessToken: string;
+}
